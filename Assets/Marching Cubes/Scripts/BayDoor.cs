0d17015c430abe2499d62278f1b3e43063f309f6
ClaytenHeuberger <claytenheuberger@gmail.com>
2025-07-27 22:27:18 -0700

    Bay Door Sound FX

--- Assets/Marching Cubes/Scripts/BayDoor.cs
+++ Assets/Marching Cubes/Scripts/BayDoor.cs
@@ -7,8 +7,15 @@ public class BayDoor : MonoBehaviour
 {
 
     [SerializeField] private GameObject bayDoor;
+    [SerializeField] private float duration = 8f;
+    [SerializeField] private float height = 6f;
+    [SerializeField] private AudioSource doorOpenAudio;
+    [SerializeField] private AudioSource doorCloseAudio;
     Vector3 startPos;
 
+
+    bool busy = false;
+    bool waiting = false;
     private void Start()
     {
         startPos = bayDoor.transform.position;
@@ -17,21 +24,29 @@ public class BayDoor : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            StopCoroutine(CloseDoor(5f));
-            StartCoroutine(OpenDoor(5f, 6f));
+            if(!busy)
+                StartCoroutine(OpenDoor(duration, height));
+            else
+                waiting = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !busy)
         {
-            StopCoroutine(OpenDoor(5f, 6f));
-            StartCoroutine(CloseDoor(5f));
+
+            if (!busy)
+                StartCoroutine(CloseDoor(duration));
+            else
+                waiting = true;
         }
     }
     IEnumerator OpenDoor(float duration, float height)
     {
+        busy = true;
+
+        doorOpenAudio.Play();
         float elapsed = 0.0f;
         Vector3 currentPos = bayDoor.transform.position;
         while(elapsed < duration)
@@ -41,9 +56,22 @@ public class BayDoor : MonoBehaviour
 
             yield return null;
         }
+
+        busy = false;
+
+        if (waiting)
+        {
+            StartCoroutine(CloseDoor(duration));
+            waiting = false;
+        }
+
     }
     IEnumerator CloseDoor(float duration)
     {
+        busy = true;
+
+        doorCloseAudio.Play();
+
         float elapsed = 0.0f;
         Vector3 currentPos = bayDoor.transform.position;
         while (elapsed < duration)
@@ -52,6 +80,15 @@ public class BayDoor : MonoBehaviour
             bayDoor.transform.position = Vector3.Lerp(currentPos, startPos, elapsed / duration);
 
             yield return null;
+        }
+
+        busy = false;
+
+
+        if (waiting)
+        {
+            StartCoroutine(OpenDoor(duration, height));
+            waiting = false;
         }
     }
 }
